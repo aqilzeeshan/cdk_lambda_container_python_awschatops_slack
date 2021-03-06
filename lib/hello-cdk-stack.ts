e695dbeb3f78ecc3797600e84086d3cdea361475
aqilzeeshan <aqilzeeshan@gmail.com>
2021-03-06 15:15:42 +0000

    Add all files

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,19 +1,43 @@
 import * as path from 'path'
 import * as Lambda from "@aws-cdk/aws-lambda"
 import * as cdk from '@aws-cdk/core';
+import * as chatbot from '@aws-cdk/aws-chatbot';
+import iam = require('@aws-cdk/aws-iam');
+
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
 // Configure path to Dockerfile
-    const dockerfile = path.join(__dirname, "../src/lambda-node-example");
+    const dockerfile = path.join(__dirname, "../src/lambda-python-example");
 
     // Create AWS Lambda function and push image to ECR
     new Lambda.DockerImageFunction(this, "function", {
       code: Lambda.DockerImageCode.fromImageAsset(dockerfile),
       timeout: cdk.Duration.seconds(300),
     });
+    
+    const slackChannel = new chatbot.SlackChannelConfiguration(this, 'MySlackChannel', {
+      slackChannelConfigurationName: 'awsbot-channel-config',
+      slackWorkspaceId: 'T01Q7BPE170',
+      slackChannelId: 'C01QE5JL4LA',
+    });
+    
+    //Issue created because the next line doesn't work https://github.com/aws/aws-cdk/issues/13444
+    //slackChannel.addLambdaInvokeCommandPermissions();
+    
+    //https://docs.aws.amazon.com/chatbot/latest/adminguide/chatbot-cli-commands.html#iam-policies-for-slack-channels-cli-support
+
+    const lambdaInvokeCommandStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        "lambda:invokeAsync",
+        "lambda:invokeFunction"
+      ],
+    });
+    slackChannel.addToRolePolicy(lambdaInvokeCommandStatement);
 
   }
 }
--- src/lambda-node-example/handler.js
+++
@@ -1,4 +0,0 @@
-const faker = require('faker')
-module.exports.lambdaHandler = async (event, context) => {
-    return faker.helpers.userCard();
-}
